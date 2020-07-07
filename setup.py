647ac7022d8782e1b3c650a702c6f5a1f21fce70
chrzyki <christoph@functor-argument.net>
2020-07-07 09:15:23 +0200

    Refactor setup.py, rebuild CLDF

--- setup.py
+++ setup.py
@@ -1,27 +1,19 @@
 from setuptools import setup
-import sys
 import json
 
 
-PY2 = sys.version_info.major == 2
-with open('metadata.json') as fp:
+with open("metadata.json") as fp:
     metadata = json.load(fp)
 
 
 setup(
-    name='lexibank_blenchbenuecongo',
-    description=metadata['title'],
-    license=metadata.get('license', ''),
-    url=metadata.get('url', ''),
-    py_modules=['lexibank_blenchbenuecongo'],
+    name="lexibank_blenchbenuecongo",
+    description=metadata["title"],
+    license=metadata.get("license", ""),
+    url=metadata.get("url", ""),
+    py_modules=["lexibank_blenchbenuecongo"],
     include_package_data=True,
     zip_safe=False,
-    entry_points={
-        'lexibank.dataset': [
-            'blenchbenuecongo=lexibank_blenchbenuecongo:Dataset',
-        ]
-    },
-    install_requires=[
-        'pylexibank>=2.0',
-    ]
+    entry_points={"lexibank.dataset": ["blenchbenuecongo=lexibank_blenchbenuecongo:Dataset",]},
+    install_requires=["pylexibank>=2.1",],
 )
